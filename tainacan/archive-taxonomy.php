5d4fc1073d35767cf43de938d20896360ad5bc16
mateuswetah <mateus.m.luna@gmail.com>
2020-12-14 12:01:41 -0300

    Adds basic items list and single item templates. #12

---
+++ tainacan/archive-taxonomy.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<?php get_template_part( 'template-parts/header-taxonomy' ); ?>
+
+	<?php tainacan_the_faceted_search(); ?>
+
+<?php get_footer();
---
+++ tainacan/single-items.php
@@ -0,0 +1,71 @@
+<?php get_header(); ?>
+
+<main id="primary" class="site-main site-container">
+
+	<?php if ( have_posts() ) : ?>
+
+		<?php do_action( 'tainacan-interface-single-item-top' ); ?>
+
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
+				
+				<?php
+					get_template_part( 'template-parts/single-items-header' );
+					do_action( 'tainacan-interface-single-item-after-title' );
+
+					echo '<div class="single-item-data-section">';
+
+					switch (get_theme_mod( 'tainacan_single_item_layout_sections_order', 'document-attachments-metadata')) {
+						case 'document-attachments-metadata':
+							get_template_part( 'template-parts/single-items-document' );
+							do_action( 'tainacan-interface-single-item-after-document' );  
+			
+							get_template_part( 'template-parts/single-items-attachments' );
+							do_action( 'tainacan-interface-single-item-after-attachments' );
+							
+							get_template_part( 'template-parts/single-items-metadata' );
+							do_action( 'tainacan-interface-single-item-after-metadata' );
+						break;
+
+						case 'metadata-document-attachments':
+							get_template_part( 'template-parts/single-items-metadata' );
+							do_action( 'tainacan-interface-single-item-after-metadata' );
+
+							get_template_part( 'template-parts/single-items-document' );
+							do_action( 'tainacan-interface-single-item-after-document' );  
+			
+							get_template_part( 'template-parts/single-items-attachments' );
+							do_action( 'tainacan-interface-single-item-after-attachments' );
+						break;
+
+						case 'document-metadata-attachments':
+							get_template_part( 'template-parts/single-items-document' );
+							do_action( 'tainacan-interface-single-item-after-document' );
+
+							get_template_part( 'template-parts/single-items-metadata' );
+							do_action( 'tainacan-interface-single-item-after-metadata' );  
+			
+							get_template_part( 'template-parts/single-items-attachments' );
+							do_action( 'tainacan-interface-single-item-after-attachments' );
+						break;
+							
+					}
+					echo '</div>';
+				?>
+
+				<?php get_template_part( 'template-parts/single-items-navigation' ); ?>
+
+				<?php get_template_part( 'template-parts/single-items-comments' ); ?>
+
+			</article>
+		<?php endwhile; ?>
+
+		<?php do_action( 'tainacan-interface-single-item-bottom' ); ?>
+
+	<?php else : ?>
+		<?php _e( 'Nothing found', 'tainacan-interface' ); ?>
+	<?php endif; ?>
+
+</main>
+
+<?php get_footer(); ?>
