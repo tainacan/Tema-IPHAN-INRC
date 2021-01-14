a36995d47e336da36627a87cf68b3b5116f18546
mateuswetah <mateus.m.luna@gmail.com>
2021-01-14 18:12:07 -0300

    Wide alignment to gutenberg blocks editor side. #14

--- functions.php
+++ functions.php
@@ -102,6 +102,13 @@ if (!function_exists('iphan_inrc_setup')) :
 				'flex-height' => true,
 			)
 		);
+
+		/* Align wide and full */
+		add_theme_support( 'align-wide' );
+
+		/* Editor Styles */
+		add_theme_support( 'editor-styles' );
+		add_editor_style( get_template_directory_uri().'/style.css' );
 	}
 endif;
 add_action('after_setup_theme', 'iphan_inrc_setup');
