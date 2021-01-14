912be4d9540ee17a5689c4d280ee9452cb189522
dvespasiano <denis.vespasiano@live.com>
2021-01-14 20:32:41 -0300

    #15 Custom logo removida

--- header.php
+++ header.php
@@ -21,6 +21,7 @@
 	<?php wp_head(); ?>
 </head>
 
+
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
@@ -33,27 +34,26 @@
 			<div class="site-container">
 				<div class="site-branding">
 					<?php
-					if (has_custom_logo()) {
-						the_custom_logo();
-					} else {
-						if (is_front_page() && is_home()) :
+					if (is_front_page() && is_home()) :
 					?>
-							<h1 class="site-title"> <?php $template_directory = get_template_directory_uri(); ?>
-								<img src="<?php echo $template_directory; ?>/assets/images/iphan_logo.png" /></h1>
-						<?php
-						else :
-						?>
-							<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"> <?php $template_directory = get_template_directory_uri(); ?>
-									<img src="<?php echo $template_directory; ?>/assets/images/iphan_logo.png" /></a></h1>
+						<h1 class="site-title"> <?php $template_directory = get_template_directory_uri(); ?>
+							<img src="<?php echo $template_directory; ?>/assets/images/iphan_logo.png" />
+						</h1>
 					<?php
-						endif;
-					} ?>
+					else :
+					?>
+						<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"> <?php $template_directory = get_template_directory_uri(); ?>
+								<img src="<?php echo $template_directory; ?>/assets/images/iphan_logo.png" /></a></h1>
+					<?php
+					endif;
+					?>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" aria-label="<?php esc_html_e('Primary Menu', 'iphan_inrc'); ?>">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8.745 5.841" height="22.075" width="33.05">
-							<path d="M.002 4.855l8.731.02-.002.966L0 5.82zm.006-2.433l8.731.02-.002.977L.006 3.4zM8.745.02l-.002.966L.01.966.013 0z" fill="#fff" /></svg>
+							<path d="M.002 4.855l8.731.02-.002.966L0 5.82zm.006-2.433l8.731.02-.002.977L.006 3.4zM8.745.02l-.002.966L.01.966.013 0z" fill="#fff" />
+						</svg>
 					</button>
 					<?php
 					wp_nav_menu(
