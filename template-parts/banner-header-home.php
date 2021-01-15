3a542fd9acda4594db697c6e5e61fa182853f080
dvespasiano <denis.vespasiano@live.com>
2021-01-15 20:34:37 -0300

    #11 Banner colorido na home page e banner preto e branco nas demais telas

--- template-parts/banner-header-home.php
+++ template-parts/banner-header-home.php
@@ -3,7 +3,7 @@
 function custom_banner_home()
 {
     $template_directory = get_template_directory_uri();
-    echo '<h1 class="site-title"><img src="' . $template_directory . '/assets/images/banner.jpg"/></h1>';
+    echo '<h1 class="site-title"><img src="' . $template_directory . '/assets/images/banner-home.png"/></h1>';
     echo '<div class="text-banner">';
     echo '<span class="title-banner">inventário </br></span>';
     echo '<span class="title-banner">nacional</span>';
