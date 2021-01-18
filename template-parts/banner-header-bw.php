d0a5755495f47fc9a4d921f7aa479b23e8c980e7
dvespasiano <denis.vespasiano@live.com>
2021-01-18 15:52:49 -0300

    #11 Correção de menu mobile

--- template-parts/banner-header-bw.php
+++ template-parts/banner-header-bw.php
@@ -3,7 +3,8 @@
 function custom_banner_bw()
 {
     $template_directory = get_template_directory_uri();
-    echo '<h1 class="site-title banner"><img src="' . $template_directory . '/assets/images/banner-bw.jpg"/></h1>';
+    /*     echo '<h1 class="site-title banner"><img src="' . $template_directory . '/assets/images/banner-bw.jpg"/></h1>'; */
+    echo '<h1 class="site-title banner-bw"></h1>';
     echo '<div class="text-banner text-banner-bw site-container">';
     echo '<span class="title-banner">inventário </br></span>';
     echo '<span class="title-banner">nacional</span>';
--- template-parts/banner-header-home.php
+++ template-parts/banner-header-home.php
@@ -3,7 +3,8 @@
 function custom_banner_home()
 {
     $template_directory = get_template_directory_uri();
-    echo '<h1 class="site-title banner"><img src="' . $template_directory . '/assets/images/banner-home.png"/></h1>';
+    /*     echo '<h1 class="site-title banner-home"><img src="' . $template_directory . '/assets/images/banner-home.png"/></h1>'; */
+    echo '<h1 class="site-title banner-home"></h1>';
     echo '<div class="text-banner site-container">';
     echo '<span class="title-banner">inventário </br></span>';
     echo '<span class="title-banner">nacional</span>';
