07c63b1f8b4db2621b8d0b70244f24f9846c7a19
dvespasiano <denis.vespasiano@live.com>
2021-01-18 16:11:36 -0300

    #11 Ajuste de imagens

--- template-parts/banner-header-bw.php
+++ template-parts/banner-header-bw.php
@@ -4,7 +4,7 @@ function custom_banner_bw()
 {
     $template_directory = get_template_directory_uri();
     /*     echo '<h1 class="site-title banner"><img src="' . $template_directory . '/assets/images/banner-bw.jpg"/></h1>'; */
-    echo '<div class="container-banner"><h1 class="site-title banner-bw"></h1></div>';
+    echo '<div class="container-banner"><h1 class="site-title banner-bw" style="background-image: url(' . $template_directory . '/assets/images/banner-bw.jpg)"></h1></div>';
     echo '<div class="text-banner text-banner-bw site-container">';
     echo '<span class="title-banner">inventário </br></span>';
     echo '<span class="title-banner">nacional</span>';
