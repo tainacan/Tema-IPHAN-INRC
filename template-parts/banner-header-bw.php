e6ac3342fa27ff172688f574d0e62e9dff1198d9
dvespasiano <denis.vespasiano@live.com>
2021-01-18 14:55:58 -0300

    #11 Ajuste da container do título da página

--- template-parts/banner-header-bw.php
+++ template-parts/banner-header-bw.php
@@ -4,7 +4,7 @@ function custom_banner_bw()
 {
     $template_directory = get_template_directory_uri();
     echo '<h1 class="site-title banner"><img src="' . $template_directory . '/assets/images/banner-bw.jpg"/></h1>';
-    echo '<div class="text-banner text-banner-bw">';
+    echo '<div class="text-banner text-banner-bw site-container">';
     echo '<span class="title-banner">inventário </br></span>';
     echo '<span class="title-banner">nacional</span>';
     echo '</div>';
--- template-parts/banner-header-home.php
+++ template-parts/banner-header-home.php
@@ -4,7 +4,7 @@ function custom_banner_home()
 {
     $template_directory = get_template_directory_uri();
     echo '<h1 class="site-title banner"><img src="' . $template_directory . '/assets/images/banner-home.png"/></h1>';
-    echo '<div class="text-banner">';
+    echo '<div class="text-banner site-container">';
     echo '<span class="title-banner">inventário </br></span>';
     echo '<span class="title-banner">nacional</span>';
     echo '</div>';
