4659b43a33fe0bad8c3564e357529aa31d9759ad
mateuswetah <mateus.m.luna@gmail.com>
2021-01-25 15:23:31 -0300

    Fixes to the menu and banner css.

--- header.php
+++ header.php
@@ -68,7 +68,8 @@
 		</header><!-- #masthead -->
 
 
-    	<div class="container-banner site-titlee" style="background-image: url(' <?php header_image() ?> )">
+		<div class="container-banner site-title" style="background-image: url(' <?php header_image() ?> )">
+			<div class="container-banner__overlay"></div>
 			<div class="text-banner site-container">
 				<span class="title-banner"><span class="title-banner__text">inventário</span></span>
 				<br>
