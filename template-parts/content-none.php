85f6e7de2c3506ac6540ef656714975c7c910a8f
dvespasiano <denis.vespasiano@live.com>
2021-02-02 09:52:01 -0300

    #14 Alihamento de página de busca

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found site-content entry-content">
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'iphan_inrc' ); ?></h1>
 	</header><!-- .page-header -->
