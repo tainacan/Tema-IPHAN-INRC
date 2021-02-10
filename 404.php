d7f1d17b45e3edb23c56696cbdd761e53173357f
dvespasiano <denis.vespasiano@live.com>
2021-02-10 15:22:55 -0300

    #14 Alinhamento de itens página 404

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main ">
+	<main id="primary" class="site-main entry-content">
 
 		<section class="error-404 not-found">
 			<header class="page-header">
