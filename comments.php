b64bdfd5ee66fe018e07f1bf82c99f70ab9180e4
dvespasiano <denis.vespasiano@live.com>
2021-02-11 15:57:35 -0300

    #14 Alteração de formato de data

--- comments.php
+++ comments.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying comments
  *
@@ -15,7 +16,7 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
+if (post_password_required()) {
 	return;
 }
 ?>
@@ -24,23 +25,23 @@ if ( post_password_required() ) {
 
 	<?php
 	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
+	if (have_comments()) :
+	?>
 		<h2 class="comments-title">
 			<?php
 			$iphan_inrc_comment_count = get_comments_number();
-			if ( '1' === $iphan_inrc_comment_count ) {
+			if ('1' === $iphan_inrc_comment_count) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'iphan_inrc' ),
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+					esc_html__('One thought on &ldquo;%1$s&rdquo;', 'iphan_inrc'),
+					'<span>' . wp_kses_post(get_the_title()) . '</span>'
 				);
 			} else {
-				printf( 
+				printf(
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $iphan_inrc_comment_count, 'comments title', 'iphan_inrc' ) ),
-					number_format_i18n( $iphan_inrc_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+					esc_html(_nx('%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $iphan_inrc_comment_count, 'comments title', 'iphan_inrc')),
+					number_format_i18n($iphan_inrc_comment_count), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					'<span>' . wp_kses_post(get_the_title()) . '</span>'
 				);
 			}
 			?>
@@ -50,6 +51,12 @@ if ( post_password_required() ) {
 
 		<ol class="comment-list">
 			<?php
+			add_filter('get_comment_date', 'wp_comment_date');
+			function wp_comment_date($date)
+			{
+				$date = date("m/d/Y");
+				return $date;
+			}
 			wp_list_comments(
 				array(
 					'style'      => 'ol',
@@ -64,10 +71,10 @@ if ( post_password_required() ) {
 		the_comments_navigation();
 
 		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'iphan_inrc' ); ?></p>
-			<?php
+		if (!comments_open()) :
+		?>
+			<p class="no-comments"><?php esc_html_e('Comments are closed.', 'iphan_inrc'); ?></p>
+	<?php
 		endif;
 
 	endif; // Check for have_comments().
