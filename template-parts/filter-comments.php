fe990042e8d52779cd3f990799fe105fd5263d1a
dvespasiano <denis.vespasiano@live.com>
2021-02-11 17:11:17 -0300

    #14 remoção de campos não utilizados

--- template-parts/filter-comments.php
+++ template-parts/filter-comments.php
@@ -1,16 +1,30 @@
 <?php
 // Alterar ordem dos campos dos comentários
-add_filter( 'comment_form_fields', 'comment_fields_custom_order' );
-function comment_fields_custom_order( $fields ) {
+add_filter('comment_form_fields', 'comment_fields_custom_order');
+function comment_fields_custom_order($fields)
+{
 	$comment_field = $fields['comment'];
 	$author_field = $fields['author'];
-	unset( $fields['comment'] );
-	unset( $fields['author'] );
-	unset( $fields['email'] );
-	unset( $fields['url'] );
+	unset($fields['comment']);
+	unset($fields['author']);
+	unset($fields['email']);
+	unset($fields['url']);
 	// the order of fields is the order below, change it as needed:
 	$fields['author'] = $author_field;
 	$fields['comment'] = $comment_field;
 	// done ordering, now return the fields:
 	return $fields;
 }
+?>
+
+<script>
+	jQuery(function($) {
+		var commentEmail = $('textarea#email');
+		commentEmail.removeAttr('required'); // remove required attribute of textarea.
+		$('#commentform').on('submit', function() {
+			if (commentEmail.val() == '') {
+				commentEmail.css('text-indent', '-999px').val('%dummy-text%');
+			}
+		});
+	});
+</script>
