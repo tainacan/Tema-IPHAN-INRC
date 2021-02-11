1c86c650e8eff042af7436b5838496e147d8174e
mateuswetah <mateus.m.luna@gmail.com>
2021-02-11 10:25:11 -0300

    Calls breadcrumbs and banner on evry page except for certain templates.

---
+++ template-parts/site-banner.php
@@ -0,0 +1,8 @@
+<div class="container-banner site-title" style="background-image: url(' <?php header_image() ?> )">
+    <div class="container-banner__overlay"></div>
+    <div class="text-banner site-container">
+        <span class="title-banner"><span class="title-banner__text">inventário</span></span>
+        <br>
+        <span class="title-banner"><span class="title-banner__text">nacional</span></span>
+    </div>
+</div>
