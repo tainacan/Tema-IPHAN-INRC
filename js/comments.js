84677c36822c890f44a95140385c72f616f84be0
dvespasiano <denis.vespasiano@live.com>
2021-02-17 15:04:12 -0300

    #14 Instruções try catch em funções jquery para evitar erros

--- js/comments.js
+++ js/comments.js
@@ -8,43 +8,65 @@ jQuery(function ($) {
 
 // corrige bug do ícone nos replys dos comments
 jQuery(function ($) {
-    var commentReply = document.getElementsByClassName('comment-reply-link');
-    for (let i = 0; i < commentReply.length; i++) {
-        commentReply[i].innerText = '';
-        commentReply[i].innerHTML = '<i size="50px" class="tainacan-icon tainacan-icon-undo"></i>';
+    try {
+        var commentReply = document.getElementsByClassName('comment-reply-link');
+        for (let i = 0; i < commentReply.length; i++) {
+            commentReply[i].innerText = '';
+            commentReply[i].innerHTML = '<i size="50px" class="tainacan-icon tainacan-icon-undo"></i>';
+        }
+    } catch {
+
     }
 });
 
 // troca o texto do botão submit do comment
 jQuery(function ($) {
-    var commentReplyButton = document.getElementById('submit');
-    commentReplyButton.value = "Enviar";
+    try {
+        var commentReplyButton = document.getElementById('submit');
+        commentReplyButton.value = "Enviar";
+    } catch {
+
+    }
 });
 
 // troca o texto "Comentário" por "Mensagem" e adiciona placeholder
 jQuery(function ($) {
-    var commentTextLabel = jQuery("label[for='comment']")
-    commentTextLabel[0].innerText = "Mensagem";
+    try {
+        var commentTextLabel = jQuery("label[for='comment']")
+        commentTextLabel[0].innerText = "Mensagem";
+    } catch {
+
+    }
 });
 
 //adiciona placeholder's
 jQuery(function ($) {
-    var commentTextArea = document.getElementById("comment");
-    commentTextArea.setAttribute('placeholder', "Digite aqui seu comentário");
-    var nome = document.getElementById("author");
-    nome.setAttribute('placeholder', 'Digite aqui...');
+    try {
+        var commentTextArea = document.getElementById("comment");
+        commentTextArea.setAttribute('placeholder', "Digite aqui seu comentário");
+        var nome = document.getElementById("author");
+        nome.setAttribute('placeholder', 'Digite aqui...');
+    } catch {
+
+    }
 })
 
 /// Adicionar collapse aos comments
 /* jQuery(function ($) {
+    try{
     jQuery('.children').addClass('collapse');
+    }catch{
+
+    }
 }); */
 
 // Add ou remove a classe 'show' ao clicar
 /* jQuery('comment-reply-link').click(function () {
+    try{
     if (jQuery('children').hasClass('show')) {
         jQuery('children').addClass('show');
     } else {
         jQuery('children').removeClass('show');
     }
+}catch{}
 }); */
