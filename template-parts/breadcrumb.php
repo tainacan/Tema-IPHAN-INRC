7d4371b393ea0c5c9bc2d0261cea791c31693a7a
mateuswetah <mateus.m.luna@gmail.com>
2021-02-26 08:45:01 -0300

    Translate some breadcrumb labels

--- template-parts/breadcrumb.php
+++ template-parts/breadcrumb.php
@@ -4,7 +4,7 @@ function custom_breadcrumbs()
 {
     $showOnHome = 0; // 1 - show breadcrumbs on the homepage, 0 - don't show
     $delimiter = '<span class="separator">/</span>'; // delimiter between crumbs
-    $home = __('Home', 'tainacan-interface'); // text for the 'Home' link
+    $home = __('Início', 'iphan_inrc'); // text for the 'Home' link
     $showCurrent = 1; // 1 - show current post/page title in breadcrumbs, 0 - don't show
     $before = '<span class="current text-black">'; // tag before the current crumb
     $after = '</span>'; // tag after the current crumb
@@ -22,9 +22,9 @@ function custom_breadcrumbs()
         if (is_category()) {
             $thisCat = get_category(get_query_var('cat'), false);
             if ($thisCat->parent != 0) echo get_category_parents($thisCat->parent, TRUE, '&nbsp;' . $delimiter . '&nbsp;');
-            echo $before . __('Archive by category "', 'tainacan-interface') . single_cat_title('', false) . '"' . $after;
+            echo $before . __('Listagem da categoria  "', 'iphan_inrc') . single_cat_title('', false) . '"' . $after;
         } elseif (is_search()) {
-            echo $before . __('Search results for "', 'tainacan-interface') . get_search_query() . '"' . $after;
+            echo $before . __('Resultados de busca para "', 'iphan_inrc') . get_search_query() . '"' . $after;
         } elseif (is_day()) {
             echo '<a href="' . esc_url(get_year_link(get_the_time('Y'))) . '">' . get_the_time('Y') . '</a>' . $delimiter . '&nbsp;';
             echo '<a href="' . esc_url(get_month_link(get_the_time('Y'), get_the_time('m'))) . '">' . get_the_time('F') . '</a>' . $delimiter . '&nbsp;';
@@ -40,7 +40,7 @@ function custom_breadcrumbs()
                 $slug = $post_type->rewrite;
                 if (!is_page() && get_post_type() != 'tainacan-collection') {
                     echo '<a href="' . esc_url(get_post_type_archive_link('tainacan-collection')) . '">';
-                    _e('Collections', 'tainacan-interface');
+                    _e('Coleções', 'iphan_inrc');
                     echo '</a>&nbsp;' . $delimiter . '&nbsp;';
                 }
                 echo '<a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->singular_name . '</a>';
@@ -85,7 +85,7 @@ function custom_breadcrumbs()
                 echo $before . $term->name . $after;
             } elseif (!is_tax() && get_post_type() != 'tainacan-collection') {
                 echo '<a href="' . esc_url(get_post_type_archive_link('tainacan-collection')) . '">';
-                _e('Collections', 'tainacan-interface');
+                _e('Coleções', 'iphan_inrc');
                 echo '</a>&nbsp;' . $delimiter . '&nbsp;';
 
                 echo $before . $post_type->labels->singular_name . $after;
@@ -116,18 +116,18 @@ function custom_breadcrumbs()
             }
             if ($showCurrent == 1) echo '&nbsp;' . $delimiter . '&nbsp;' . $before . get_the_title() . $after;
         } elseif (is_tag()) {
-            echo $before . __('Posts tagged "', 'tainacan-interface') . single_tag_title('', false) . '"' . $after;
+            echo $before . __('Publicações marcadas "', 'iphan_inrc') . single_tag_title('', false) . '"' . $after;
         } elseif (is_author()) {
             global $author;
             $userdata = get_userdata($author);
-            echo $before . __('Articles posted by ', 'tainacan-interface') . $userdata->display_name . $after;
+            echo $before . __('Artigos publicados por ', 'iphan_inrc') . $userdata->display_name . $after;
         } elseif (is_404()) {
-            echo $before . __('Error 404', 'tainacan-interface') . $after;
+            echo $before . __('Erro 404', 'iphan_inrc') . $after;
         }
 
         if (get_query_var('paged')) {
             if (is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author()) echo '&nbsp;(';
-            echo __('Page', 'tainacan-interface') . '&nbsp;' . get_query_var('paged');
+            echo __('Page', 'iphan_inrc') . '&nbsp;' . get_query_var('paged');
             if (is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author()) echo ')';
         }
 
