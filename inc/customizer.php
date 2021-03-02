947a7ec5c90a8cdeb88d72dce345fe5cf433e887
dvespasiano <denis.vespasiano@live.com>
2021-03-02 15:13:51 -0300

    #32 Adição de campos

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * IPHAN INRC Theme Customizer
  *
@@ -10,22 +11,23 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function iphan_inrc_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+function iphan_inrc_customize_register($wp_customize)
+{
+    $wp_customize->get_setting('blogname')->transport         = 'postMessage';
+    $wp_customize->get_setting('header_textcolor')->transport = 'postMessage';
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial(
-			'blogname',
-			array(
-				'selector'        => '.site-title a',
-				'render_callback' => 'iphan_inrc_customize_partial_blogname',
-			)
-		);
-	}
+    if (isset($wp_customize->selective_refresh)) {
+        $wp_customize->selective_refresh->add_partial(
+            'blogname',
+            array(
+                'selector'        => '.site-title a',
+                'render_callback' => 'iphan_inrc_customize_partial_blogname',
+            )
+        );
+    }
 
-	/* REDES SOCIAIS E RODAPÉ */
-	$wp_customize->add_section('social_section', array(
+    /* REDES SOCIAIS E RODAPÉ */
+    $wp_customize->add_section('social_section', array(
         'title'       => __('Redes Sociais', 'Tema_IPHAN_INRC'),
     ));
 
@@ -101,8 +103,7 @@ function iphan_inrc_customize_register( $wp_customize ) {
         'settings' => 'setting_alt_4',
         'priority' => 4,
     ));
-    $wp_customize->add_setting('iphan_logo_4', array(
-    ));
+    $wp_customize->add_setting('iphan_logo_4', array());
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'iphan_logo_control_4', array(
         'label' => 'Upload de imagem da nova rede social 1',
         'priority' => 1,
@@ -191,22 +192,59 @@ function iphan_inrc_customize_register( $wp_customize ) {
             'change' => 'Alterar Imagem',
         )
     )));
+
+    /* LINK DO BANNER */
+    $wp_customize->add_section('link_banner', array(
+        'title'       => __('Banner', 'Tema_IPHAN_INRC'),
+    ));
+
+    /* Link Banner 1 */
+    $wp_customize->add_setting('setting_link_banner_1', array(
+        'default' => '',
+        'type' => 'theme_mod',
+        'transport'  => 'postMessage',
+        'sanitize_callback' => 'esc_url_raw'
+    ));
+    $wp_customize->add_control('link_1', array(
+        'label' => 'Link do lado Esquerdo',
+        'type' => 'text',
+        'section' => 'link_banner',
+        'settings' => 'setting_link_banner_1',
+        'priority' => 1,
+    ));
+
+    /* Link Banner 2 */
+    $wp_customize->add_setting('setting_link_banner_2', array(
+        'default' => '',
+        'type' => 'theme_mod',
+        'transport'  => 'postMessage',
+        'sanitize_callback' => 'esc_url_raw'
+    ));
+    $wp_customize->add_control('link_2', array(
+        'label' => 'Link do lado Direito',
+        'type' => 'text',
+        'section' => 'link_banner',
+        'settings' => 'setting_link_banner_2',
+        'priority' => 1,
+    ));
 }
-add_action( 'customize_register', 'iphan_inrc_customize_register' );
+add_action('customize_register', 'iphan_inrc_customize_register');
 
 /**
  * Render the site title for the selective refresh partial.
  *
  * @return void
  */
-function iphan_inrc_customize_partial_blogname() {
-	bloginfo( 'name' );
+function iphan_inrc_customize_partial_blogname()
+{
+    bloginfo('name');
 }
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function iphan_inrc_customize_preview_js() {
-	wp_enqueue_script( 'iphan_inrc-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), IPHAN_INRC_VERSION, true );
+function iphan_inrc_customize_preview_js()
+{
+    wp_enqueue_script('iphan_inrc-customizer', get_template_directory_uri() . '/js/customizer.js', array('customize-preview'), IPHAN_INRC_VERSION, true);
 }
-add_action( 'customize_preview_init', 'iphan_inrc_customize_preview_js' );
+add_action('customize_preview_init', 'iphan_inrc_customize_preview_js');
