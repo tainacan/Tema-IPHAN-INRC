a1f8aaf211669877de5696c50a97ebe55f3d3cae
dvespasiano <denis.vespasiano@live.com>
2021-03-09 14:44:56 -0300

    #42 Correções de GRID de notícias

--- template-parts/content.php
+++ template-parts/content.php
@@ -26,35 +26,37 @@ if (!is_singular()) {
 					<?php
 					if (is_singular()) :
 						iphan_inrc_post_thumbnail();
-						the_title('<div"><a class="mb-3> ', '</a></div>');
+						the_title('<div class="alignwide"><h3 class="mb-3 is-style-title-iphan-underscore"> ', '</h3></div>');
 					else :
-						the_title('<div><a class="font-weight-bold mb-3" href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></div>');
+						the_title('<div><a class="font-weight-bold mb-3 is-style-title-iphan" href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></div>');
 					endif;
-
-					if ('post' === get_post_type()) :
 					?>
-						<div class="entry-meta">
-							<?php
-							iphan_inrc_posted_on();
-							iphan_inrc_posted_by();
-							?>
-						</div><!-- .entry-meta -->
-					<?php endif; ?>
 				</header><!-- .entry-header -->
-
 				<p class="margin-list-post">
 					<?php
-					echo excerpt(35)
+					if (is_singular()) {
+						echo '<div class="alignwide">';
+						the_content();
+						echo '</div>';
+					} else {
+						echo '<div class="alignwide">';
+						echo excerpt(35);
+						echo '</div>';
+					}
 					?>
 				</p><!-- .entry-content -->
-				<div class="info-footer-resume margin-list-post">
-					<div class="date-excerpt-position">
-						<span class="date-excerpt"><?php echo get_the_date("j") . " de " . get_the_date("F, Y") ?></span>
+				<?php if (!is_singular()) {
+				?>
+					<div class="info-footer-resume margin-list-post">
+						<div class="date-excerpt-position">
+							<span class="date-excerpt"><?php echo get_the_date("j") . " de " . get_the_date("F, Y") ?></span>
+						</div>
+						<div class="wp-block-button">
+							<a href="<?php the_permalink(); ?>" class="wp-block-button__link"><?php _e('Ler Mais <i size="50px" class="tainacan-icon tainacan-icon-next"></i>', 'ler-mais'); ?></a>
+						</div>
 					</div>
-					<div class="wp-block-button">
-						<a href="<?php the_permalink(); ?>" class="wp-block-button__link"><?php _e('Ler Mais <i size="50px" class="tainacan-icon tainacan-icon-next"></i>', 'ler-mais'); ?></a>
-					</div>
-				</div>
+				<?php } ?>
+
 			</div>
 			</article>
 			<?php
