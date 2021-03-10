88b9040301bec5259e4ff5003b2226d51ec931de
dvespasiano <denis.vespasiano@live.com>
2021-03-10 13:52:33 -0300

    #26 Correção de erros de inicialização do bloco

--- custom-blocks/accordion-block/accordion-block.php
+++ custom-blocks/accordion-block/accordion-block.php
@@ -1,31 +1,31 @@
 <?php
-function accordion_block_init() {
-
+function accordion_block_init()
+{
     // automatically load dependencies and version
-    $asset_file = include(plugin_dir_path(__FILE__). 'build/index.asset.php');
+/*     $asset_file = include(plugin_dir_path(__FILE__) . 'block.asset.php'); */
 
     wp_register_script(
         'accordion_block-editor',
         plugins_url('block.js', __FILE__),
         $asset_file['dependencies'],
-        $asset_file['version'] 
+        $asset_file['version']
     );
 
     wp_register_style(
         'accordion_block-editor',
         plugins_url('editor.css', __FILE__),
         array(),
-        filemtime(plugin_dir_path(__FILE__). 'editor.css')
+        filemtime(plugin_dir_path(__FILE__) . 'editor.css')
     );
 
     wp_register_style(
         'accordion_block',
         plugins_url('style.css', __FILE__),
         array(),
-        filemtime(plugin_dir_path(__FILE__). 'style.css')
+        filemtime(plugin_dir_path(__FILE__) . 'style.css')
     );
 
-    register_block_type('custom/accordion_block', array(
+    register_block_type('accordion_block', array(
         'editor_script' => 'accordion_block-editor',
         'editor_style'  => 'accordion_block-editor',
         'style'         => 'accordion_block',
