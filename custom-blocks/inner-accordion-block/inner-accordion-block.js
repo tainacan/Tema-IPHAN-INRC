b2d016c6055e2cf7b17e8d514a858a7e9ffbe814
dvespasiano <denis.vespasiano@live.com>
2021-03-16 09:42:06 -0300

    #26 Geração de blocos dentro de blocos com a tag details do html

--- custom-blocks/inner-accordion-block/inner-accordion-block.js
+++ custom-blocks/inner-accordion-block/inner-accordion-block.js
@@ -10,64 +10,25 @@ wp.blocks.registerBlockType(
         title: 'Inner block',
         icon: 'arrow-down',
         category: 'text',
-        attributes: {
-            title: {
-                type: 'string',
-                source: 'html',
-                selector: 'summary',
-            },
-            content: {
-                type: 'string',
-                source: 'html',
-                selector: 'div',
-            },
-        },
 
-        edit: function (props) {
-            var title = props.attributes.title;
-            function updateTitle(newTitle) {
-                props.setAttributes({ title: newTitle });
-            }
-            /*             var content = props.attributes.content;
-                        function onChangeConteudo(novoConteudo) {
-                            props.setAttributes({ content: novoConteudo });
-                        } */
+        edit: function () {
             var blockProps = useBlockProps();
-            return el('details',
-                RichText,
-                [el(
-                    RichText,
-                    {
-                        value: props.attributes.title,
-                        tagName: 'summary',
-                        type: 'text',
-                        onChange: updateTitle,
-                        value: title,
-                        placeholder: "Insira o título"
-                    }
-                ),
-                el(
-                    blockProps,
-                    el(InnerBlocks)
-                )]
+
+            return el(
+                'details',
+                blockProps,
+                el(InnerBlocks)
             );
         },
-        save: function (props) {
-            var blockProps = wp.blockEditor.useBlockProps.save();
-            return (el('details',
+
+        save: function () {
+            var blockProps = useBlockProps.save();
+
+            return el(
+                'details',
                 blockProps,
-                [el(
-                    RichText.Content,
-                    {
-                        tagName: 'summary',
-                        className: 'tainacan-icon tainacan-icon-showmore',
-                        value: props.attributes.title
-                    }),
-                el(
-                    blockProps,
-                    el(InnerBlocks.Content)
-                )]
-            ))
+                el(InnerBlocks.Content)
+            );
         },
     }
 );
