5aa488709a05bc417881834b3c00effbf023472f
dvespasiano <denis.vespasiano@live.com>
2021-03-26 12:04:39 -0300

    #14 Limpeza e padronização de código

--- inc/imports.php
+++ inc/imports.php
@@ -2,33 +2,28 @@
 
 /* ----------------------------- INC IMPORTS  ----------------------------- */
 
-/**
- * Implement the Custom Header feature.
- */
+
+//Implement the Custom Header feature.
 require get_template_directory() . '/inc/custom-header.php';
 
-/**
- * Custom template tags for this theme.
- */
+
+//Custom template tags for this theme.
 require get_template_directory() . '/inc/template-tags.php';
 
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
+
+//Functions which enhance the theme by hooking into WordPress.
 require get_template_directory() . '/inc/template-functions.php';
 
 
 //excerpts
 require get_template_directory() . '/inc/excerpts.php';
 
-/**
- * Customizer additions.
- */
+
+//Customizer additions.
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
+
+//Load Jetpack compatibility file.
 if (defined('JETPACK__VERSION')) {
     require get_template_directory() . '/inc/jetpack.php';
 }
