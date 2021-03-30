18c2a9ca143b8dae912d03dfb8c929b463005774
dvespasiano <denis.vespasiano@live.com>
2021-03-30 17:09:36 -0300

    #51 Revisão de tradução de strings

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -12,7 +12,7 @@
 
 <section class="no-results not-found site-content entry-content">
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e('Nothing Found', 'iphan-inrc'); ?></h1>
+		<h1 class="page-title"><?php esc_html_e('Nothing Found', 'iphan_inrc'); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
@@ -22,7 +22,7 @@
 			printf(
 				'<p>' . wp_kses(
 					/* translators: 1: link to WP admin new post page. */
-					__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'iphan-inrc'),
+					__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'iphan_inrc'),
 					array(
 						'a' => array(
 							'href' => array(),
@@ -33,9 +33,9 @@
 			);
 
 		elseif (is_search()) : ?>
-			<p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'iphan-inrc'); ?></p>
+			<p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'iphan_inrc'); ?></p>
 		<?php else : ?>
-			<p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'iphan-inrc'); ?></p>
+			<p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'iphan_inrc'); ?></p>
 		<?php endif; ?>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
