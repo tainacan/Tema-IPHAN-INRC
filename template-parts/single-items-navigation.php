88cbaf35981525f7980a97cba9f187b325de93bb
dvespasiano <denis.vespasiano@live.com>
2021-03-30 17:05:22 -0300

    #51 revisão de strings traduzíveis

--- template-parts/single-items-navigation.php
+++ template-parts/single-items-navigation.php
@@ -1,25 +1,25 @@
-<?php 
+<?php
 
-    $adjacent_links = [
-        'next' => '',
-        'previous' => ''
-    ];
+$adjacent_links = [
+    'next' => '',
+    'previous' => ''
+];
 
-    $adjacent_links = tainacan_get_adjacent_item_links();
+$adjacent_links = tainacan_get_adjacent_item_links();
 
-    $previous = $adjacent_links['previous'];
-    $next = $adjacent_links['next'];
+$previous = $adjacent_links['previous'];
+$next = $adjacent_links['next'];
 ?>
 <?php if ($previous !== '' || $next !== '') : ?>
 
     <hr class="alignfull" style="height: 3px; background: #58020b;" />
-    
+
     <div class="site-container tainacan-single-item-navigation">
 
-        <?php if ( get_theme_mod('tainacan_single_item_navigation_section_label', __( 'Continue explorando', 'iphan_inrc' )) != '') : ?>
+        <?php if (get_theme_mod('tainacan_single_item_navigation_section_label', __('Continue explorando', 'iphan_inrc')) != '') : ?>
             <div class="is-style-title-iphan-underscore title-page">
                 <h1 id="single-item-navigation-label">
-                    <?php echo esc_html( get_theme_mod( 'tainacan_single_item_navigation_section_label', __('Continue explorando', 'iphan_inrc') ) ); ?>
+                    <?php echo esc_html(get_theme_mod('tainacan_single_item_navigation_section_label', __('Continue explorando', 'iphan_inrc'))); ?>
                 </h1>
             </div>
         <?php endif; ?>
@@ -31,8 +31,8 @@
             <div class="related-post">
                 <a style="background-image: url(<?php echo 'alguma_url' ?>)" rel="next" href="<?php echo tainacan_get_source_item_list_url() ?>">
                     <div class="post-box"><img src="' . $next_thumb . '" alt=""' . $next_title . '">
-                        <span class="post-type"><?php echo __('Itens do Inventário') ?></span>
-                        <span class="post-title"><?php echo 'Nome do Inventário de Origem' ?><span>
+                        <span class=" post-type"><?php echo __('Itens do Inventário', 'iphan_inrc') ?></span>
+                        <span class="post-title"><?php echo __('Nome do Inventário de Origem', 'iphan_inrc') ?><span>
                     </div>
                 </a>
             </div>
