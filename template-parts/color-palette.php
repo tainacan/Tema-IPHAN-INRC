c00d5e90029cb2f6151286b8d90a50aa76d90264
dvespasiano <denis.vespasiano@live.com>
2021-03-30 16:32:37 -0300

    #51 Revisão de strings traduzíveis

--- template-parts/color-palette.php
+++ template-parts/color-palette.php
@@ -1,89 +1,88 @@
 <?php
 add_theme_support('editor-color-palette', array(
     array(
-        'nome' => __('Branco', 'textdomain'),
+        'nome' => __('Branco', 'textdomain', 'iphan-inrc'),
         'slug' => 'branco',
         'color' => '#FFFFFF',
     ),
     array(
-        'name' => __('Preto', 'textdomain'),
+        'name' => __('Preto', 'textdomain', 'iphan-inrc'),
         'slug' => 'preto',
         'color' => '#000000',
     ),
     array(
-        'name' => __('Vermelho', 'textdomain'),
+        'name' => __('Vermelho', 'textdomain', 'iphan-inrc'),
         'slug' => 'vermelho',
         'color' => '#58020b',
     ),
     array(
-        'name' => __('Vermelho 2', 'textdomain'),
+        'name' => __('Vermelho 2', 'textdomain', 'iphan-inrc'),
         'slug' => 'vermelho2',
         'color' => '#970620',
     ),
     array(
-        'name' => __('Amarelo', 'textdomain'),
+        'name' => __('Amarelo', 'textdomain', 'iphan-inrc'),
         'slug' => 'amarelo',
         'color' => '#f3d7a2',
     ),
     array(
-        'name' => __('Amarelo 2', 'textdomain'),
+        'name' => __('Amarelo 2', 'textdomain', 'iphan-inrc'),
         'slug' => 'amarelo2',
         'color' => '#fcd9aa',
     ),
     array(
-        'name' => __('Verde', 'textdomain'),
+        'name' => __('Verde', 'textdomain', 'iphan-inrc'),
         'slug' => 'verde',
         'color' => '#007350',
     ),
     array(
-        'name' => __('Laranja', 'textdomain'),
+        'name' => __('Laranja', 'textdomain', 'iphan-inrc'),
         'slug' => 'laranja',
         'color' => '#ca4d37',
     ),
     array(
-        'name' => __('Amarelo 3', 'textdomain'),
+        'name' => __('Amarelo 3', 'textdomain', 'iphan-inrc'),
         'slug' => 'amarelo3',
         'color' => '#e5a923',
     ),
     array(
-        'name' => __('Branco 1', 'textdomain'),
+        'name' => __('Branco 1', 'textdomain', 'iphan-inrc'),
         'slug' => 'branco1',
         'color' => '#f5f5f5',
     ),
     array(
-        'name' => __('Cinza 1', 'textdomain'),
+        'name' => __('Cinza 1', 'textdomain', 'iphan-inrc'),
         'slug' => 'cinza1',
         'color' => '#f1f1f1',
     ),
     array(
-        'name' => __('Cinza 2', 'textdomain'),
+        'name' => __('Cinza 2', 'textdomain', 'iphan-inrc'),
         'slug' => 'cinza2',
         'color' => '#e8e8e8',
     ),
     array(
-        'name' => __('Cinza 3', 'textdomain'),
+        'name' => __('Cinza 3', 'textdomain', 'iphan-inrc'),
         'slug' => 'cinza3',
         'color' => '#d9d9d9',
     ),
     array(
-        'name' => __('Cinza 4', 'textdomain'),
+        'name' => __('Cinza 4', 'textdomain', 'iphan-inrc'),
         'slug' => 'cinza4',
         'color' => '#bfbfbf',
     ),
     array(
-        'name' => __('Cinza 5', 'textdomain'),
+        'name' => __('Cinza 5', 'textdomain', 'iphan-inrc'),
         'slug' => 'cinza5',
         'color' => '#898d8f',
     ),
     array(
-        'name' => __('Cinza 6', 'textdomain'),
+        'name' => __('Cinza 6', 'textdomain', 'iphan-inrc'),
         'slug' => 'cinza6',
         'color' => '#515151',
     ),
     array(
-        'name' => __('Cinza 7', 'textdomain'),
+        'name' => __('Cinza 7', 'textdomain', 'iphan-inrc'),
         'slug' => 'cinza7',
         'color' => '#1e1e1e',
     ),
 ));
-?>
