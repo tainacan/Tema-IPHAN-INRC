dc4d34be4cb0d980aad10e27cdb47a640d528d95
dvespasiano <denis.vespasiano@live.com>
2021-03-30 16:28:14 -0300

    #51 Revisão de string traduzível

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -27,7 +27,7 @@ if ( ! function_exists( 'iphan_inrc_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'iphan_inrc' ),
+			esc_html_x( 'Posted on %s', 'post date', 'iphan-inrc' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
@@ -43,7 +43,7 @@ if ( ! function_exists( 'iphan_inrc_posted_by' ) ) :
 	function iphan_inrc_posted_by() {
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'iphan_inrc' ),
+			esc_html_x( 'by %s', 'post author', 'iphan-inrc' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
@@ -60,17 +60,17 @@ if ( ! function_exists( 'iphan_inrc_entry_footer' ) ) :
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'iphan_inrc' ) );
+			$categories_list = get_the_category_list( esc_html__( ', ', 'iphan-inrc' ) );
 			if ( $categories_list ) {
 				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'iphan_inrc' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'iphan-inrc' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			}
 
 			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'iphan_inrc' ) );
+			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'iphan-inrc' ) );
 			if ( $tags_list ) {
 				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'iphan_inrc' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'iphan-inrc' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			}
 		}
 
@@ -80,7 +80,7 @@ if ( ! function_exists( 'iphan_inrc_entry_footer' ) ) :
 				sprintf(
 					wp_kses(
 						/* translators: %s: post title */
-						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'iphan_inrc' ),
+						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'iphan-inrc' ),
 						array(
 							'span' => array(
 								'class' => array(),
@@ -97,7 +97,7 @@ if ( ! function_exists( 'iphan_inrc_entry_footer' ) ) :
 			sprintf(
 				wp_kses(
 					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'iphan_inrc' ),
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'iphan-inrc' ),
 					array(
 						'span' => array(
 							'class' => array(),
