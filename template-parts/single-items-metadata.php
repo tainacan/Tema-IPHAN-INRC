41b655e0d54ffd2715349f902c552bd1ff7cb952
dvespasiano <denis.vespasiano@live.com>
2021-03-31 09:31:48 -0300

    #51 Revisão de strings traduzpiveis

--- template-parts/single-items-metadata.php
+++ template-parts/single-items-metadata.php
@@ -1,23 +1,23 @@
 <section class="tainacan-content tainacan-item-section__metadata">
-    <?php if ( get_theme_mod('tainacan_single_item_metadata_section_label', __( 'Informações', 'iphan_inrc' )) != '') : ?>
+    <?php if (get_theme_mod('tainacan_single_item_metadata_section_label', __('Informações', 'iphan_inrc')) != '') : ?>
         <h2 class="tainacan-single-item-section" id="single-item-metadata-label">
-            <?php echo esc_html( get_theme_mod('tainacan_single_item_metadata_section_label', __('Informações', 'iphan_inrc')) ); ?>
+            <?php echo esc_html(get_theme_mod('tainacan_single_item_metadata_section_label', __('Informações', 'iphan_inrc'))); ?>
         </h2>
     <?php endif; ?>
-    
+
     <div class="single-item-collection--information">
-        <?php do_action( 'iphan-inrc-single-item-metadata-begin' ); ?>
+        <?php do_action('iphan-inrc-single-item-metadata-begin'); ?>
         <?php
-            $args = array(
-                'before_title' => '<div><h3>',
-                'after_title' => '</h3>',
-                'before_value' => '<p>',
-                'after_value' => '</p></div>',
-                'exclude_title' => true
-            );
-            //$field = null;
-            tainacan_the_metadata( $args );
+        $args = array(
+            'before_title' => '<div><h3>',
+            'after_title' => '</h3>',
+            'before_value' => '<p>',
+            'after_value' => '</p></div>',
+            'exclude_title' => true
+        );
+        //$field = null;
+        tainacan_the_metadata($args);
         ?>
-        <?php do_action( 'iphan-inrc-single-item-metadata-end' ); ?>
+        <?php do_action('iphan-inrc-single-item-metadata-end'); ?>
     </div>
 </section>
