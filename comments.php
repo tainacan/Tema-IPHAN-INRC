5ac08662fe1a9f7f944d86101f3e99f2da811efb
dvespasiano <denis.vespasiano@live.com>
2021-03-31 09:24:03 -0300

    #51 Revisão de strings traduzíveis

--- comments.php
+++ comments.php
@@ -20,9 +20,7 @@ if (post_password_required()) {
 	return;
 }
 ?>
-
 <div id="comments" class="comments-area">
-
 	<?php
 	// You can start editing here -- including this comment!
 	if (have_comments()) :
@@ -46,7 +44,6 @@ if (post_password_required()) {
 			}
 			?>
 		</h2><!-- .comments-title -->
-
 		<?php the_comments_navigation(); ?>
 		<ol class="comment-list">
 			<?php
@@ -69,19 +66,14 @@ if (post_password_required()) {
 			);
 			?>
 		</ol><!-- .comment-list -->
-
 		<?php
-
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if (!comments_open()) :
 		?>
 			<p class="no-comments"><?php esc_html_e('Comments are closed.', 'iphan_inrc'); ?></p>
 	<?php
 		endif;
-
 	endif; // Check for have_comments().
-
 	comment_form();
 	?>
-
 </div><!-- #comments -->
