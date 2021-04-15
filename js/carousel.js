6fdf0756d38f4945be9a4f381ff33411f5d0f553
dvespasiano <denis.vespasiano@live.com>
2021-04-15 11:34:23 -0300

    #52 Correção de colunas e de background image do carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,15 @@
 jQuery(document).on('ready', function () {
     const swiper = new Swiper('.swiper-container', {
         // Optional parameters
-        direction: 'vertical',
+        direction: 'horizontal',
         loop: false,
+        slidesPerView: 1,
+        spaceBetween: 4,
         // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+
     });
 })
