0f23c3d8e54f3b0e829958bec5cb22956379fe78
dvespasiano <denis.vespasiano@live.com>
2021-04-16 15:58:28 -0300

    #19 Correção de tamanhos e quantidades de slides

--- js/carousel.js
+++ js/carousel.js
@@ -27,12 +27,12 @@ jQuery(document).on('ready', function () {
                 slidesPerView: 1,
                 spaceBetween: 20
             },
-            // when window width is >= 480px
+            // when window width is >= 768px
             768: {
                 slidesPerView: 2,
                 spaceBetween: 30
             },
-            // when window width is >= 640px
+            // when window width is >= 1024px
             1024: {
                 slidesPerView: 3,
                 spaceBetween: 40
