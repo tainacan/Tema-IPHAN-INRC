08a13f44419ff16e6365381ab2d4e7f12a24cc8e
dvespasiano <denis.vespasiano@live.com>
2021-04-16 11:23:57 -0300

    #19 Confecção de carousel para os posts relacionados

--- js/carousel.js
+++ js/carousel.js
@@ -13,3 +13,30 @@ jQuery(document).on('ready', function () {
 
     });
 })
+jQuery(document).on('ready', function () {
+    const swiper = new Swiper('.swiper-container-relacionados', {
+        // Default parameters
+        slidesPerView: 1,
+        spaceBetween: 10,
+        direction: 'horizontal',
+        loop: false,
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            // when window width is >= 640px
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 40
+            }
+        }
+    })
+})
--- template-parts/content-singular.php
+++ template-parts/content-singular.php
@@ -75,9 +75,83 @@
 	?>
 		<hr class="hr-posts-relacionados">
 		</hr>
+		<?php
+		$defaults = array(
+			'numberposts'      => 5,
+			'offset'           => 0,
+			'category'         => 0,
+			'orderby'          => 'post_date',
+			'order'            => 'DESC',
+			'include'          => '',
+			'exclude'          => '',
+			'meta_key'         => '',
+			'meta_query' => array(
+				array(
+					'key'     => '_thumbnail_id',
+					'value'   => '',
+					'compare' => '!=',
+				)
+			),
+			'meta_value'       => '',
+			'post_type'        => 'post',
+			'post_status'      => 'draft, publish, future',
+			'suppress_filters' => true,
+		);
+		$results = get_posts($defaults);
+		?>
 		<div class="posts-relacionados entry-content col-md-12">
 			<h1 class="is-style-title-iphan-underscore">relacionados</h1>
-			<div>
+			<div class="swiper-container-relacionados">
+				<div class="swiper-wrapper">
+					<a class="col-sm-4 col-md-4" href="<?php echo get_post_permalink($results[0]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[0]->ID) ?>')">
+						<div class="destaques-content">
+							<?php
+							echo '<span class="destaques-cat">' . get_the_category($results[0]->ID)[0]->cat_name . '</span>';
+							echo '<span class="destaques-title">' . get_the_title($results[0]) . '</span>';
+							?>
+						</div>
+					</a>
+					<a class="col-sm-4 col-md-4" href="<?php echo get_post_permalink($results[1]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[1]->ID) ?>')">
+						<div class="destaques-content">
+							<?php
+							echo '<span class="destaques-cat">' . get_the_category($results[1]->ID)[0]->cat_name . '</span>';
+							echo '<span class="destaques-title">' . get_the_title($results[1]) . '</span>';
+							?>
+						</div>
+					</a>
+					<a class="col-sm-4 col-md-4" href="<?php echo get_post_permalink($results[2]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[2]->ID) ?>')">
+						<div class="destaques-content">
+							<?php
+							echo '<span class="destaques-cat">' . get_the_category($results[2]->ID)[0]->cat_name . '</span>';
+							echo '<span class="destaques-title">' . get_the_title($results[2]) . '</span>';
+							?>
+						</div>
+					</a>
+					<a class="col-sm-4 col-md-4" href="<?php echo get_post_permalink($results[0]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[3]->ID) ?>')">
+						<div class="destaques-content">
+							<?php
+							echo '<span class="destaques-cat">' . get_the_category($results[3]->ID)[0]->cat_name . '</span>';
+							echo '<span class="destaques-title">' . get_the_title($results[3]) . '</span>';
+							?>
+						</div>
+					</a>
+					<a class="col-sm-4 col-md-4" href="<?php echo get_post_permalink($results[4]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[4]->ID) ?>')">
+						<div class="destaques-content">
+							<?php
+							echo '<span class="destaques-cat">' . get_the_category($results[4]->ID)[4]->cat_name . '</span>';
+							echo '<span class="destaques-title">' . get_the_title($results[4]) . '</span>';
+							?>
+						</div>
+					</a>
+					<a class="col-sm-4 col-md-4" href="<?php echo get_post_permalink($results[5]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[5]->ID) ?>')">
+						<div class="destaques-content">
+							<?php
+							echo '<span class="destaques-cat">' . get_the_category($results[5]->ID)[0]->cat_name . '</span>';
+							echo '<span class="destaques-title">' . get_the_title($results[5]) . '</span>';
+							?>
+						</div>
+					</a>
+				</div>
 			</div>
 		</div>
 	<?php
