1e1e7517407d2767aebf2b9b080af0642c74d7d8
dvespasiano <denis.vespasiano@live.com>
2021-04-23 11:48:33 -0300

    #20 Correção de erros do carousel em relação a responsividade

--- js/carousel.js
+++ js/carousel.js
@@ -35,6 +35,11 @@ jQuery(document).on('ready', function () {
                 slidesPerView: 3,
                 spaceBetween: 20
             }
-        }
+        },
+                // Navigation arrows
+                navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     })
 })
