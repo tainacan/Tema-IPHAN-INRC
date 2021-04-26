5b3698520e1716d8179b1bcfdf77495a93693955
dvespasiano <denis.vespasiano@live.com>
2021-04-26 19:58:57 -0300

    #12 Montagem de display grid

--- template-parts/destaques-home.php
+++ template-parts/destaques-home.php
@@ -28,82 +28,17 @@
             <?php _e('Destaques', 'iphan_inrc') ?>
         </h1>
     </div>
-    <div class="linha-1-destaques col-md-12">
+    <div class="linha-1-destaques grid-container col-md-12">
         <?php
-        if ($results[0] !== null) {
+        for ($i = 0; $i < count($results); $i++) {
         ?>
-            <a class="col-sm-12 col-md-8" href="<?php echo get_post_permalink($results[0]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[0]->ID) ?>')">
+            <a class="display-<?php echo $i + 1 ?>" href="<?php echo get_post_permalink($results[$i]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[$i]->ID) ?>')">
                 <div class="destaques-content">
                     <?php
-                    echo '<span class="destaques-cat">' . get_the_category($results[0]->ID)[0]->cat_name . '</span>';
-                    echo '<span class="destaques-title">' . get_the_title($results[0]) . '</span>';
+                    echo '<span class="destaques-cat">' . get_the_category($results[$i]->ID)[0]->cat_name . '</span>';
+                    echo '<span class="destaques-title">' . get_the_title($results[$i]) . '</span>';
                     ?>
                 </div>
             </a>
         <?php } ?>
-        <?php
-        if ($results[1] !== null) {
-        ?>
-            <a class="col-sm-12 col-md-4" href="<?php echo get_post_permalink($results[1]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[1]->ID) ?>')">
-                <div class="destaques-content">
-                    <?php
-                    echo '<span class="destaques-cat">' . get_the_category($results[1]->ID)[0]->cat_name . '</span>';
-                    echo '<span class="destaques-title">' . get_the_title($results[1]) . '</span>';
-                    ?>
-                </div>
-            </a>
-        <?php } ?>
-    </div>
-    <div class="linha-2-destaques destaque-bottom">
-        <?php
-        if ($results[2] !== null) {
-        ?>
-            <a class="col-sm-12 col-md-4" href="<?php echo get_post_permalink($results[2]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[2]->ID) ?>')">
-                <div class="destaques-content">
-                    <?php
-                    echo '<span class="destaques-cat">' . get_the_category($results[2]->ID)[0]->cat_name . '</span>';
-                    echo '<span class="destaques-title">' . get_the_title($results[2]) . '</span>';
-                    ?>
-                </div>
-            </a>
-        <?php } ?>
-        <?php
-        if ($results[3] !== null) {
-        ?>
-            <a class="col-sm-12 col-md-4" href="<?php echo get_post_permalink($results[3]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[3]->ID) ?>')">
-                <div class="destaques-content">
-                    <?php
-                    echo '<span class="destaques-cat">' . get_the_category($results[3]->ID)[0]->cat_name . '</span>';
-                    echo '<span class="destaques-title">' . get_the_title($results[3]) . '</span>';
-                    ?>
-                </div>
-            </a>
-        <?php } ?>
-        <div class="is-mobile col-md-4">
-            <?php
-            if ($results[4] !== null) {
-            ?>
-                <a class="col-md-6 destaque-bottom-right" href="<?php echo get_post_permalink($results[4]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[4]->ID) ?>')">
-                    <div class="destaques-content">
-                        <?php
-                        echo '<span class="destaques-cat">' . get_the_category($results[4]->ID)[0]->cat_name . '</span>';
-                        echo '<span class="destaques-title">' . get_the_title($results[4]) . '</span>';
-                        ?>
-                    </div>
-                </a>
-            <?php } ?>
-            <?php
-            if ($results[5] !== null) {
-            ?>
-                <a class="col-md-6 destaque-bottom-right" href="<?php echo get_post_permalink($results[5]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[5]->ID) ?>')">
-                    <div class="destaques-content">
-                        <?php
-                        echo '<span class="destaques-cat">' . get_the_category($results[5]->ID)[0]->cat_name . '</span>';
-                        echo '<span class="destaques-title">' . get_the_title($results[5]) . '</span>';
-                        ?>
-                    </div>
-                </a>
-            <?php } ?>
-        </div>
-    </div>
 </section>
--- template-parts/destaques-noticias.php
+++ template-parts/destaques-noticias.php
@@ -29,72 +29,18 @@
         </h1>
     </div>
     <div class="div-destaques-noticias col-md-12 is-desktop">
-        <div class="col-md-5">
-            <?php
-            if ($results[0] !== null) {
-            ?>
-                <a class="col-sm-12 col-md-12" href="<?php echo get_post_permalink($results[0]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[0]->ID) ?>')">
-                    <div class="destaques-content">
-                        <?php
-                        echo '<span class="destaques-cat">' . get_the_category($results[0]->ID)[0]->cat_name . '</span>';
-                        echo '<span class="destaques-title">' . get_the_title($results[0]) . '</span>';
-                        ?>
-                    </div>
-                </a>
-            <?php } ?>
-            <?php
-            if ($results[1] !== null) {
-            ?>
-                <a class="col-sm-12 col-md-12" href="<?php echo get_post_permalink($results[1]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[1]->ID) ?>')">
-                    <div class="destaques-content">
-                        <?php
-                        echo '<span class="destaques-cat">' . get_the_category($results[1]->ID)[0]->cat_name . '</span>';
-                        echo '<span class="destaques-title">' . get_the_title($results[1]) . '</span>';
-                        ?>
-                    </div>
-                </a>
-            <?php } ?>
-        </div>
-        <div class="col-md-3">
-            <?php
-            if ($results[2] !== null) {
-            ?>
-                <a class="col-sm-12 col-md-12 destaques-noticias-mid" href="<?php echo get_post_permalink($results[0]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[2]->ID) ?>')">
-                    <div class="destaques-content">
-                        <?php
-                        echo '<span class="destaques-cat">' . get_the_category($results[2]->ID)[0]->cat_name . '</span>';
-                        echo '<span class="destaques-title">' . get_the_title($results[2]) . '</span>';
-                        ?>
-                    </div>
-                </a>
-            <?php } ?>
-        </div>
-        <div class="col-md-4">
-            <?php
-            if ($results[3] !== null) {
-            ?>
-                <a class="col-sm-12 col-md-12" href="<?php echo get_post_permalink($results[0]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[3]->ID) ?>')">
-                    <div class="destaques-content">
-                        <?php
-                        echo '<span class="destaques-cat">' . get_the_category($results[3]->ID)[0]->cat_name . '</span>';
-                        echo '<span class="destaques-title">' . get_the_title($results[3]) . '</span>';
-                        ?>
-                    </div>
-                </a>
-            <?php } ?>
-            <?php
-            if ($results[4] !== null) {
-            ?>
-                <a class="col-sm-12 col-md-12" href="<?php echo get_post_permalink($results[1]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[4]->ID) ?>')">
-                    <div class="destaques-content">
-                        <?php
-                        echo '<span class="destaques-cat">' . get_the_category($results[4]->ID)[0]->cat_name . '</span>';
-                        echo '<span class="destaques-title">' . get_the_title($results[4]) . '</span>';
-                        ?>
-                    </div>
-                </a>
-            <?php } ?>
-        </div>
+        <?php
+        for ($i = 0; $i < count($results); $i++) {
+        ?>
+            <a class="col-sm-12 col-md-12" href="<?php echo get_post_permalink($results[$i]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[$i]->ID) ?>')">
+                <div class="destaques-content">
+                    <?php
+                    echo '<span class="destaques-cat">' . get_the_category($results[$i]->ID)[0]->cat_name . '</span>';
+                    echo '<span class="destaques-title">' . get_the_title($results[$i]) . '</span>';
+                    ?>
+                </div>
+            </a>
+        <?php } ?>
     </div>
     <div class="is-mobile-noticias swiper-container col-sm-12">
         <div class="swiper-wrapper col-sm-12">
