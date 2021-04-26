f17b00c541a1d54b349ddd126cb4a10e9d4e3c57
dvespasiano <denis.vespasiano@live.com>
2021-04-26 19:05:37 -0300

    #19 Melhora e limpeza de código

--- home.php
+++ home.php
@@ -36,11 +36,6 @@ custom_breadcrumbs();
 			/* Start the Loop */
 			while (have_posts()) :
 				the_post();
-				/*
-						* Include the Post-Type-specific template for the content.
-						* If you want to override this in a child theme, then include a file
-						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-						*/
 				get_template_part('template-parts/content', 'list');
 			endwhile;
 			the_posts_pagination(array(
