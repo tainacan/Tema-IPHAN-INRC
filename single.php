76c66aa1f69adceb3e63c2690d486c314ac69801
dvespasiano <denis.vespasiano@live.com>
2021-04-26 21:11:35 -0300

    #12 Remoção dos posts navigation

--- single.php
+++ single.php
@@ -20,13 +20,13 @@ custom_breadcrumbs();
 		the_post();
 
 		get_template_part('template-parts/content', 'singular');
-
-		the_post_navigation(
-			array(
-				'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'iphan_inrc') . '</span> <span class="nav-title">%title</span>',
-				'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'iphan_inrc') . '</span> <span class="nav-title">%title</span>',
-			)
-		);
+		// Remoção dos posts navigation
+		/* 		the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'iphan_inrc') . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'iphan_inrc') . '</span> <span class="nav-title">%title</span>',
+				)
+			); */
 		// If comments are open or we have at least one comment, load up the comment template.
 		if (comments_open() || get_comments_number()) :
 			comments_template();
