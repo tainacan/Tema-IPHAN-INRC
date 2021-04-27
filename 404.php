0361da95350ca27aa7c9fe1434ef6ac65f326e95
dvespasiano <denis.vespasiano@live.com>
2021-04-26 22:53:18 -0300

    #53 Adequação de tradução

--- 404.php
+++ 404.php
@@ -45,7 +45,7 @@ custom_breadcrumbs();
 			</div><!-- .widget -->
 			<?php
 			/* translators: %1$s: smiley */
-			$iphan_inrc_archive_content = '<p>' . esc_html__('Buscar por data', 'iphan_inrc') . '</p>';
+			$iphan_inrc_archive_content = '<p>' . esc_html__('Busque por data', 'iphan_inrc') . '</p>';
 			the_widget('WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$iphan_inrc_archive_content");
 			the_widget('WP_Widget_Tag_Cloud');
 			?>
