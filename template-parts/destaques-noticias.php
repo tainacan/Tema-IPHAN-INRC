6ee029c92ee99f2b3370768cdd1929df980addf1
dvespasiano <denis.vespasiano@live.com>
2021-04-28 20:41:02 -0300

    #53 Destaques Noticias has 5

--- template-parts/destaques-noticias.php
+++ template-parts/destaques-noticias.php
@@ -28,11 +28,11 @@
             <?php _e('Notícias', 'iphan_inrc') ?>
         </h1>
     </div>
-    <div class="div-destaques-noticias grid-container-noticias col-md-12 is-desktop">
+    <div class="div-destaques-noticias grid-container-noticias grid-container-noticias-has-<?php echo count($results) ?> col-md-12 is-desktop">
         <?php
         for ($i = 0; $i < count($results); $i++) {
         ?>
-            <a class="display-<?php echo $i + 1 ?>-noticias" href="<?php echo get_post_permalink($results[$i]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[$i]->ID) ?>')">
+            <a class="display-<?php echo $i + 1 ?>-noticias-has-<?php echo count($results) ?>" href="<?php echo get_post_permalink($results[$i]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[$i]->ID) ?>')">
                 <div class="destaques-content">
                     <?php
                     echo '<span class="destaques-cat">' . get_the_category($results[$i]->ID)[0]->cat_name . '</span>';
