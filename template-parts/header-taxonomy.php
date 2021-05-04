e4d6757debfc54cd3119126fdaecccd17a6965a3
mateuswetah <mateus.m.luna@gmail.com>
2021-05-04 11:34:26 -0300

    Adicionatitulo e descriçao para as listagens de item do termo.

--- template-parts/header-taxonomy.php
+++ template-parts/header-taxonomy.php
@@ -6,6 +6,18 @@
  * It will only work with tainacan plugin enabled.
  *
  */
+$current_wpterm = tainacan_get_term();
+
 ?>
 <!-- Get the banner to display -->
-<?php get_template_part('template-parts/headertaxonomy'); ?>
+<div class="row alignfull">
+    <div class="col col-sm-6 col-11">
+        <h2 class="is-style-title-iphan-underscore"><?php tainacan_the_term_name();?></h2>
+        <?php $tainacan_term_description = tainacan_the_term_description(); ?>
+        <?php if (!empty($tainacan_term)) : ?>
+            <p>
+                <?php tainacan_the_term_description(); ?>
+            </p>
+        <?php endif; ?>
+    </div>
+</div>
