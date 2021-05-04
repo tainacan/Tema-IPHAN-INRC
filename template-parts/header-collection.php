1f7b1e1c0adff1e55cd2627afcf4818c62368d8c
mateuswetah <mateus.m.luna@gmail.com>
2021-05-04 11:30:49 -0300

    Corrige css das listagens do Tainacan

--- template-parts/header-collection.php
+++ template-parts/header-collection.php
@@ -8,7 +8,7 @@
  */
 ?>
 <div class="row alignfull">
-    <div class="col col-6 col-sm-12">
+    <div class="col col-sm-6 col-11">
         <h2 class="is-style-title-iphan-underscore"><?php tainacan_the_collection_name(); ?></h2>
         <?php $tainacan_collection_description = tainacan_get_the_collection_description(); ?>
         <?php if (!empty($tainacan_collection_description)) : ?>
