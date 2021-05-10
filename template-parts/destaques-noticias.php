36248696d85960236661e4f5592d1cb99483641d
dvespasiano <denis.vespasiano@live.com>
2021-05-10 20:27:41 -0300

    #64 Alteração do titulo do bloco de notícias

--- template-parts/destaques-noticias.php
+++ template-parts/destaques-noticias.php
@@ -23,21 +23,25 @@
         );
         $results = get_posts($defaults);
         ?>
-        <div class="titulo-destaques">
-            <h1 class="is-style-title-iphan-underscore">
-                <?php _e('Notícias', 'iphan_inrc') ?>
-            </h1>
-        </div>
-        <div class="div-destaques-noticias grid-container-noticias grid-container-noticias-has-<?php if (count($results) <= 4) {
+        <div class="div-destaques-noticias grid-container-noticias grid-container-noticias-has-<?php if (count($results) + 1 <= 3) {
                                                                                                     echo count($results);
                                                                                                 } else {
                                                                                                     echo '5';
                                                                                                 } ?> col-md-12 is-desktop">
+            <div class="titulo-destaques display-1-noticias-has-<?php if (count($results) + 1 <= 4) {
+                                                                    echo count($results) + 1;
+                                                                } else {
+                                                                    echo 5;
+                                                                } ?>">
+                <h1 class="is-style-title-iphan-underscore">
+                    <?php _e('Notícias', 'iphan_inrc') ?>
+                </h1>
+            </div>
             <?php
-            for ($i = 0; $i < count($results) && $i < 5; $i++) {
+            for ($i = 0; $i < count($results) && $i < 4; $i++) {
             ?>
-                <a class="display-<?php echo $i + 1 ?>-noticias-has-<?php if (count($results) <= 4) {
-                                                                        echo count($results);
+                <a class="display-<?php echo $i + 2 ?>-noticias-has-<?php if (count($results) + 1 <= 4) {
+                                                                        echo count($results) + 1;
                                                                     } else {
                                                                         echo 5;
                                                                     } ?>" href="<?php echo get_post_permalink($results[$i]->ID) ?>" style="background-image: url(' <?php echo get_the_post_thumbnail_url($results[$i]->ID) ?>')">
