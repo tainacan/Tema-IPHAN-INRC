db70157a5e85d67f9f8a7ed1a2b95fa3a55b341a
dvespasiano <denis.vespasiano@live.com>
2021-05-12 19:44:38 -0300

    #58 diversas alterações nas nomeclaturas dos arquivos

--- custom-blocks/card-block/card-block.php
+++ custom-blocks/card-block/card-block.php
@@ -9,22 +9,22 @@ function card_block_init()
 
     wp_register_style(
         'card_block-IPHAN-editor',
-        plugins_url('editor.css', __FILE__),
+        plugins_url('editor-card.css', __FILE__),
         array(),
-        filemtime(plugin_dir_path(__FILE__) . 'editor.scss')
+        filemtime(plugin_dir_path(__FILE__) . 'editor-card.scss')
     );
 
     wp_register_style(
         'card_block_IPHAN',
-        plugins_url('style.css', __FILE__),
+        plugins_url('style-card.css', __FILE__),
         array(),
-        filemtime(plugin_dir_path(__FILE__) . 'style.scss')
+        filemtime(plugin_dir_path(__FILE__) . 'style-card.scss')
     );
 
     register_block_type('iphan/card-block-IPHAN', array(
         'editor_script' => 'card-block-IPHAN-js',
-        'editor_style'  => 'editor',
-        'style'         => 'style',
+        'editor_style'  => 'editor-card',
+        'style'         => 'style-card',
     ));
 }
 
