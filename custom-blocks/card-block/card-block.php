7fc12a7491f8c37fea4f7ba2a3f5c8dabf7781fe
dvespasiano <denis.vespasiano@live.com>
2021-05-12 20:30:37 -0300

    #58 Alteração de nomeclatura de componentes

--- custom-blocks/card-block/card-block.php
+++ custom-blocks/card-block/card-block.php
@@ -11,14 +11,14 @@ function card_block_init()
         'card_block-IPHAN-editor',
         plugins_url('editor.css', __FILE__),
         array(),
-        filemtime(plugin_dir_path(__FILE__) . 'editor-card.scss')
+        filemtime(plugin_dir_path(__FILE__) . 'editor.scss')
     );
 
     wp_register_style(
         'card_block_IPHAN',
         plugins_url('style.css', __FILE__),
         array(),
-        filemtime(plugin_dir_path(__FILE__) . 'style-card.scss')
+        filemtime(plugin_dir_path(__FILE__) . 'style.scss')
     );
 
     register_block_type('iphan/card-block-IPHAN', array(
