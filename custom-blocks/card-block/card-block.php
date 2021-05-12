e5fd7de05e00018007869301432b87961997fbb8
dvespasiano <denis.vespasiano@live.com>
2021-05-12 19:15:32 -0300

    #58 Inserção de arquivos de estilos do card block do iphan

--- custom-blocks/card-block/card-block.php
+++ custom-blocks/card-block/card-block.php
@@ -2,30 +2,30 @@
 function card_block_init()
 {
     wp_register_script(
-        'card-block-js',
+        'card-block-IPHAN-js',
         get_template_directory_uri() . '/custom-blocks/card-block/card-block.js',
         array('wp-blocks')
     );
 
     wp_register_style(
-        'inner_accordion_block-editor',
+        'card_block-IPHAN-editor',
         plugins_url('editor.css', __FILE__),
         array(),
         filemtime(plugin_dir_path(__FILE__) . 'editor.scss')
     );
 
     wp_register_style(
-        'inner_accordion_block',
+        'card_block_IPHAN',
         plugins_url('style.css', __FILE__),
         array(),
         filemtime(plugin_dir_path(__FILE__) . 'style.scss')
     );
 
-    register_block_type('iphan/inner-accordion-custom', array(
-        'editor_script' => 'inner-accordion-custom-js',
+    register_block_type('iphan/card-block-IPHAN', array(
+        'editor_script' => 'card-block-IPHAN-js',
         'editor_style'  => 'editor',
         'style'         => 'style',
     ));
 }
 
-add_action('init', 'inner_accordion_block_init');
+add_action('init', 'card_block_init');
