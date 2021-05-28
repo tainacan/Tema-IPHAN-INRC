71c95148b89965329b53b3af420644646f3b23c6
dvespasiano <denis.vespasiano@live.com>
2021-05-27 23:10:04 -0300

    #58 Traduções de nomes

--- template-parts/block-styles.php
+++ template-parts/block-styles.php
@@ -97,21 +97,21 @@ if (function_exists('register_block_style')) {
             'core/buttons',
             array(
                 'name'  => 'button-left', 'iphan_inrc',
-                'label' =>  'Button do cartão iphan alinhado a esquerda',
+                'label' =>  'Botão do cartão iphan alinhado a esquerda',
             )
         );
         register_block_style(
             'core/buttons',
             array(
                 'name'  => 'button-center', 'iphan_inrc',
-                'label' =>  'Button do cartão do iphan alinhado no centro',
+                'label' =>  'Botão do cartão do iphan alinhado no centro',
             )
         );
         register_block_style(
             'core/buttons',
             array(
                 'name'  => 'button-right', 'iphan_inrc',
-                'label' =>  'Button do cartão do iphan alinhado a direita',
+                'label' =>  'Botão do cartão do iphan alinhado a direita',
             )
         );
     }
