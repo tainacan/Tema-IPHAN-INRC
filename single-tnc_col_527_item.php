8864f3bed85ebb2a3f09c7e1053f0d3ff54bde3c
dvespasiano <denis.vespasiano@live.com>
2021-06-10 20:04:51 -0300

    #23 estilos de cabeçalhos

--- single-tnc_col_527_item.php
+++ single-tnc_col_527_item.php
@@ -1,12 +1,20 @@
-<?php
-/* Cabeçalho da página */
-get_header();
-get_template_part('template-parts/site-banner');
-custom_breadcrumbs();
+<div class="inventario-header">
+    <?php
+    /* Cabeçalho da página */
+    get_header();
+    get_template_part('template-parts/site-banner');
+    custom_breadcrumbs();
+    /* Fim do cabeçalho */
+    ?>
+</div>
 
-/* Fim do cabeçalho */
-
-echo tainacan_get_item();
-echo tainacan_get_the_metadata();
-
-echo 'the content';
+<div class="col-md-12 inventario">
+    <div class="alinhamento-template-inventarios">
+        <div class="barra-lateral">
+            <?php echo tainacan_get_the_metadata(); ?>
+        </div>
+        <div class="post-inventario">
+            <?php echo tainacan_get_item(); ?>
+        </div>
+    </div>
+</div>
