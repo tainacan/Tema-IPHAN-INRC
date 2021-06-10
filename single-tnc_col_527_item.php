ee8e1043659ea2351e3d1a4292c9425c702a7297
dvespasiano <denis.vespasiano@live.com>
2021-06-10 19:27:10 -0300

    Estruturação da página

--- single-tnc_col_527_item.php
+++ single-tnc_col_527_item.php
@@ -1,7 +1,11 @@
 <?php
-	get_header(); 
-	get_template_part('template-parts/site-banner');
-	custom_breadcrumbs(); 
+/* Cabeçalho da página */
+get_header();
+get_template_part('template-parts/site-banner');
+custom_breadcrumbs();
+
+/* Fim do cabeçalho */
+
 echo tainacan_get_item();
 echo tainacan_get_the_metadata();
 
