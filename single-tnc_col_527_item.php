985f283dccc43c612574385f058bf9e8974621d5
dvespasiano <denis.vespasiano@live.com>
2021-06-18 16:33:09 -0300

    #23 Alterações diversas para exibição dos attachments

--- single-tnc_col_527_item.php
+++ single-tnc_col_527_item.php
@@ -12,9 +12,25 @@
     <div class="alinhamento-template-inventarios">
         <div class="barra-lateral">
             <?php echo tainacan_get_the_metadata();
-            echo tainacan_get_the_attachments()
+            $attachments = tainacan_get_the_attachments();
+            /*             for ($i = 0; $i < sizeof($attachaments); $i++) {
+                tainacan_get_single_attachment_as_html($attachaments[$i]->ID);
+            } */
+            foreach ($attachments as $attachment) {
+                $media_items_thumbs[] =
+                    tainacan_get_the_media_component_slide(array(
+                        'media_content' => wp_get_attachment_image($attachment->ID, 'tainacan-medium', false),
+                        'media_content_full' => wp_attachment_is('image', $attachment->ID) ? wp_get_attachment_image($attachment->ID, 'full', false) : ('<div class="attachment-without-image tainacan-embed-container"><iframe id="tainacan-attachment-iframe" src="' . tainacan_get_attachment_html_url($attachment->ID) . '"></iframe></div>'),
+                        'media_title' => $attachment->post_title,
+                        'media_description' => $attachment->post_content,
+                        'media_caption' => $attachment->post_excerpt,
+                        'media_type' => $attachment->post_mime_type
+                    ));
+            }
+            foreach ($media_items_thumbs as $media) {
+                echo $media;
+            }
             ?>
-
         </div>
         <div class="post-inventario">
             <h1 class="is-style-title-iphan-underscore">
