f906a64adbe22ec3511a65b8f5073927e380428a
mateuswetah <mateus.m.luna@gmail.com>
2021-07-07 15:07:57 -0300

    Adds default value to archive glossary.

--- archive-glossary.php
+++ archive-glossary.php
@@ -18,7 +18,7 @@ custom_breadcrumbs();
 			<div class="entry-content alinhamento-titulos-post">
 				<?php
 				echo '<h1 class="archive-title-glossary is-style-title-iphan-underscore">' . get_theme_mod('iphan_inrc_glossary_archive_title', 'Glossário INRC') . '</h1>';
-				echo '<div class="archive-description archive-description-glossary">' . get_theme_mod('iphan_inrc_glossary_archive_description', '') . '</div>';
+				echo '<div class="archive-description archive-description-glossary">' . get_theme_mod('iphan_inrc_glossary_archive_description', 'Conheça aqui os termos do Glossário desenvolvido pelo IPHAN INRC.') . '</div>';
 				?>
 			</div>
         </header><!-- .site-container -->
